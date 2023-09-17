6a38c4daff7fd0f5753497e86176f1db554f0841
elnurabbasov123 <ahura2022@mail.ru>
2023-09-17 14:54:39 +0400

    fillCustomer updated

--- src/helper/CustomerServiceHelper.java
+++ src/helper/CustomerServiceHelper.java
@@ -7,8 +7,8 @@ public class CustomerServiceHelper {
     public static Customer fillCustomer(){
         String name= InputUtil.getInstance().inputString("Enter the name :");
         String surname= InputUtil.getInstance().inputString("Enter the surname :");
-        String passwordFin= InputUtil.getInstance().inputString("Enter the passwordFin :");
-        String phoneNumber= InputUtil.getInstance().inputString("Enter the phoneNumber :");
+        String passwordFin= InputUtil.getInstance().inputString("Enter the passwordFin:");
+        String phoneNumber= InputUtil.getInstance().inputString("Enter the phoneNumber:");
 
         Customer customer=new Customer(name,surname,passwordFin,phoneNumber);
 
